43dd873b455007a7a2b57c3b029eebf0cfd6cbaa
Jb DOYON <jb@jiby.tech>
2022-02-18 01:24:11 +0000

    Add validation of wordle guesses feature
    Tests now green, and with performance boost!

--- src/literate_wordle/words.py
+++ src/literate_wordle/words.py
@@ -2,25 +2,25 @@
 
 import gzip
 import importlib.resources as pkg_resources
+from functools import cache
 from random import choice
 from typing import Optional, Tuple
 
 from . import assets  # Relative import of the assets/ folder
 
 
-def get_words_list() -> list[str]:
-    """Decompress the wordle dictionary"""
-    dict_compressed_bytes = pkg_resources.read_binary(
-        assets, "wordle_answers_dict.txt.gz"
-    )
-    dict_string = gzip.decompress(dict_compressed_bytes).decode("ascii")
-    answer_word_list = [word.strip().lower() for word in dict_string.split("\n")]
-    return answer_word_list
+@cache
+def get_asset_zip_as_set(asset_filename: str) -> set[str]:
+    """Decompress a file in assets module into a set of words, separated by newline"""
+    compressed_bytes = pkg_resources.read_binary(assets, asset_filename)
+    string = gzip.decompress(compressed_bytes).decode("ascii")
+    string_list = [word.strip().lower() for word in string.split("\n")]
+    return set(string_list)
 
 
 def pick_answer_word() -> str:
     """Pick a single word out of the dictionary of answers"""
-    return choice(get_words_list())
+    return choice(list(get_asset_zip_as_set("wordle_answers_dict.txt.gz")))
 
 
 def check_valid_word(guess: str) -> Tuple[bool, Optional[str]]:
@@ -31,4 +31,7 @@ def check_valid_word(guess: str) -> Tuple[bool, Optional[str]]:
         return False, "Guess too short"
     elif guess_length > answer_length:
         return False, "Guess too long"
-    return True, None  # No dictionary check
+    valid_words_dict = get_asset_zip_as_set("wordle_accepted_words_dict.txt.gz")
+    if guess in valid_words_dict:
+        return True, None
+    return False, "Not a word from the dictionary"
