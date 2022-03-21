100b04b7634453e48b0616d1e9f2575fb1acbdd9
Jb DOYON <jb@jiby.tech>
2022-03-21 02:09:49 +0000

    Remove scenarios from test docstring because dupe
    As I'm trying in a separate project to regex-parse the test files for
    Gherkin keywords (ideally matching perfectly the original feature), the
    Scenarios are now duplicated between docstring and actual test code's
    comments.
    
    Remove this duplication that doesn't bring any value.

--- tests/test_scoring_guess.py
+++ tests/test_scoring_guess.py
@@ -4,43 +4,6 @@ Feature: Scoring guesses
   As a Wordle game
   I need to tell the player how good their guess is
   In order to help them find the proper answer
-
-Scenario: Perfect guess gives perfect score
-  Given a wordle answer "crane"
-  When scoring the guess "crane"
-  Then score should be "🟩🟩🟩🟩🟩"
-
-Scenario: No character in common
-  Given a wordle answer "brave"
-  When scoring the guess "skill"
-  Then score should be "⬜⬜⬜⬜⬜"
-
-Scenario: Character in wrong place
-  Given a wordle answer "rebus"
-  When scoring the guess "skull"
-  Then score should be "🟨⬜🟨⬜⬜"
-
-Scenario Outline: Scoring guesses
-  Given a wordle <answer>
-  When scoring <guess>
-  Then score should be <score>
-
-# Emoji (Unicode) character rendering is hard:
-# Please forgive the table column alignment issues!
-  Examples: A few guesses and their score
-    | answer  | guess	| score		|
-    | adage   | adobe	| 🟩🟩⬜⬜🟩	|
-    | serif   | quiet	| ⬜⬜🟨🟨⬜	|
-    | raise   | radix	| 🟩🟩⬜🟨⬜	|
-
-  Examples: Multiple occurences of same character
-    | answer | guess	| score		|
-    | abbey  | kebab	| ⬜🟨🟩🟨🟨	|
-    | abbey  | babes	| 🟨🟨🟩🟩⬜	|
-    | abbey  | abyss	| 🟩🟩🟨⬜⬜	|
-    | abbey  | algae	| 🟩⬜⬜⬜🟨	|
-    | abbey  | keeps	| ⬜🟨⬜⬜⬜	|
-    | abbey  | abate	| 🟩🟩⬜⬜🟨	|
 """
 
 import pytest
