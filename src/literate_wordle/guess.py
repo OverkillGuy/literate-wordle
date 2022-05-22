c02a23ff5b1eb58eed6cae83bdd8a28d5b91efd1
Jb DOYON <jb@jiby.tech>
2022-05-22 14:28:37 +0100

    Clean score_guess for NO
    Response as List of char defaulting to NO makes for simpler code indeed.
    Thanks @gpiancastelli for that idea!

--- src/literate_wordle/guess.py
+++ src/literate_wordle/guess.py
@@ -23,7 +23,8 @@ def score_guess(guess: str, answer: str) -> str:
     """Score an individual guess with Counter"""
     # Counter("abbey") = Counter({'b': 2, 'a': 1, 'e': 1, 'y': 1})
     answer_chars = Counter(answer)
-    response: dict[int, str] = {}
+    # NO is the default score, no need to detect it
+    response: list[str] = [CharacterScore.NO] * len(answer)
     # First pass to detect perfect scores
     perfect_scores = [
         answer_char == guess_char for guess_char, answer_char in zip(guess, answer)
@@ -32,16 +33,12 @@ def score_guess(guess: str, answer: str) -> str:
         if is_score_perfect:
             response[char_index] = CharacterScore.OK
             answer_chars[guess[char_index]] -= 1
-    # Now the non-perfects
-    for char_num, (guess_char, _answer_char) in enumerate(zip(guess, answer)):
-        if char_num in response:
-            continue  # No point scoring this one, we got it already as perfect
-        if guess_char not in answer_chars or answer_chars[guess_char] == 0:
-            response[char_num] = CharacterScore.NO
-            continue  # Early exit for this character, skip to next
-        # From here on, we MUST have a char in common, regardless of place
-        elif answer_chars[guess_char] > 0:
+    # Last, the yellows
+    for char_num, (guess_char, existing_score) in enumerate(zip(guess, response)):
+        if existing_score == CharacterScore.OK:
+            continue  # It's already green: skip
+        if guess_char in answer_chars and answer_chars[guess_char] > 0:
             response[char_num] = CharacterScore.WRONG_PLACE
-        # Either way, reduce occurence counter since we "used" this occurence
-        answer_chars[guess_char] -= 1
-    return "".join([response[i] for i in range(5)])
+            # Reduce occurence counter since we "used" this occurence
+            answer_chars[guess_char] -= 1
+    return "".join(response)
