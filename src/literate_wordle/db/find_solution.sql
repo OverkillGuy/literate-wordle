0dcde9bcc7262a41bd1e8551981242d2032639c1
Jb DOYON <jb@jiby.tech>
2022-06-24 02:35:57 +0100

    Add sample query for finding potential solution

--- src/literate_wordle/db/find_solution.sql
+++ src/literate_wordle/db/find_solution.sql
@@ -1,14 +1,16 @@
+-- Individually:
 -- Guess word:
 SELECT wordidx FROM word_index WHERE word = 'crane'; -- 2368
 -- Score returned by Wordle:
-SELECT scoreidx FROM score_index WHERE score = "⬜⬜🟨🟩⬜"; -- 15
+SELECT scoreidx FROM score_index WHERE score = '⬜⬜🟨🟩⬜'; -- 15
 -- Get answer index from guess index and score index:
 SELECT answeridx FROM scores WHERE guessidx = 2368 AND scoreidx = 15;
--- Pivoting to words:
-SELECT word_index.word
-FROM scores
-INNER JOIN word_index ON wordidx = answeridx
-WHERE guessidx = 2368 AND scoreidx = 15;
--- TODO: Replace hardcoded guess/score index with subquery
--- See this obsoleted sample:
--- -- SELECT * from score_by_secrets as s join word_by_index as w on w.wordnum = s.secret WHERE w.word = "crane";
+-- Aggregated via (sub-optimal) JOINs:
+-- Find answers for guess "crane" given its wordle score
+SELECT answer.word FROM scores AS s
+JOIN score_index AS si ON s.scoreidx = si.scoreidx
+JOIN word_index as guess ON guess.wordidx = s.guessidx
+JOIN word_index as answer ON answer.wordidx = s.answeridx
+WHERE guess.word = 'crane' AND si.score = '⬜⬜🟨🟩⬜';
+-- FIXME: Answer is picked from the ACCEPTED words list, not the subset that
+-- matches the answers table!
