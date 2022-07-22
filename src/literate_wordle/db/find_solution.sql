a3582f45c9c5f7f17b6755da252c5bc3b22616fa
Jb DOYON <jb@jiby.tech>
2022-07-22 02:28:04 +0100

    Add entropy-calculating SQL samples
    Now the sqlite in Python supports math operators, we can fill in
    entropy via SQL operations, rather than compute in pure Python.
    
    This is because crucial math func log2 is now available in pysqlite3.
    
    Unfortunately I'm limited by my own personal knowledge of SQL now.
    Great project to learn though, I'm really enjoying this low-stakes deep
    dive into SQL with tangible benefits at the end, even if I'm just
    recreating the results of 3blue1brown at the end: Building this on top
    of literate_wordle is nice bonus.

--- src/literate_wordle/db/find_solution.sql
+++ src/literate_wordle/db/find_solution.sql
@@ -14,3 +14,30 @@ JOIN word_index as answer ON answer.wordidx = s.answeridx
 WHERE guess.word = 'crane' AND si.score = '⬜⬜🟨🟩⬜';
 -- FIXME: Answer is picked from the ACCEPTED words list, not the subset that
 -- matches the answers table!
+
+
+-- Calculating entropy
+
+
+SELECT count(*) FROM word_index; -- total number of accepted words: 12897
+SELECT count(*) FROM answers_index; -- total number of answer words: 2315
+
+-- Calculate the Information-Theory Entropy of a probability
+-- Formula: log2(1/probability), with probability = float(num_occurences) / total
+-- For an outcome happening 124 times over all answer words:
+SELECT log2(1/ (124. /12897) ) AS entropy;
+
+-- For solving Wordle, we want:
+-- What is the number of answers that match this score for that guess
+-- Aka what is the entropy = log 2 (1 / (number_valid_answers / total_num_answers))
+
+-- Storing entropy as a table:
+CREATE TABLE score_probability (
+       guessidx int,
+       scoreidx int,
+       number_occurences int,
+       entropy float,
+       FOREIGN KEY(guessidx) REFERENCES word_index(wordidx),
+       FOREIGN KEY(scoreidx) REFERENCES score_index(scoreidx)
+);
+-- TODO: Populate table, iterating over score_index but grouped by scoreidx
